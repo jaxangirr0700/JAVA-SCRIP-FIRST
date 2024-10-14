a8799bf5e87c4174a48c396ec8f8859e45e1ffdf
jaxangir <jaxangirr0700@gmail.com>
2024-10-14 14:41:50 +0500

    finished!

---
+++ main.js
@@ -0,0 +1,70 @@
+// 1-MISOL BOSHLANISHI
+
+// let name=prompt("Enter your name");
+// let surname=prompt("Enter your surname");
+// let email=prompt("Enter your is email");
+// let age=prompt("Enter your age");
+// let city=prompt("Enter your city");
+// let telephone=prompt("Enter your telephone number");
+// let from=prompt("Enter your from");
+// let nation=prompt("Enter your nation");
+// let ismarried=prompt("Enter your is married");
+// let interest=prompt("Enter your is interest");
+
+// console.log(`
+//     name:${name},
+//     surname:${surname},
+//     email:${email},
+//     age:${age},
+//     city:${city},
+//     telephone:${telephone},
+//     from:${from},
+//     nation:${nation},
+//     ismarried:${ismarried},
+//     interest:${interest},
+//     `);
+
+//     alert(`
+//             name:${name},
+//     surname:${surname},
+//     email:${email},
+//     age:${age},
+//     city:${city},
+//     telephone:${telephone},
+//     from:${from},
+//     nation:${nation},
+//     ismarried:${ismarried},
+//     interest:${interest},
+//         `)
+
+// 1-MISOL TUGASHI
+
+// 2-MISOL BOSHLANISHI
+
+// let olma = +prompt("omla narxi!");
+// let nok = +prompt("nok narxi!");
+// let banan = +prompt("banan narxi!");
+// let qulupnay = +prompt("qulupnay narxi!");
+// let ananas = +prompt("ananas narxi!");
+// let narxlarjamlanmasi = olma+nok+banan+qulupnay+ananas;
+
+
+// console.log(narxlarjamlanmasi);
+// alert("narxlar yig'indisi! ="+narxlarjamlanmasi)
+
+// 2-MISOL TUGASHI
+
+// 3-MISOL BOSHLANISHI
+
+// let olma = +prompt("omla narxi!");
+// let nok = +prompt("nok narxi!");
+// let banan = +prompt("banan narxi!");
+// let qulupnay = +prompt("qulupnay narxi!");
+// let ananas = +prompt("ananas narxi!");
+// let narxlarjamlanmasi = (olma+nok+banan+qulupnay+ananas)/5;
+
+
+// console.log(narxlarjamlanmasi);
+// alert("narxlar yig'indisi! ="+narxlarjamlanmasi)
+
+// 3-MISOL TUGASHI
